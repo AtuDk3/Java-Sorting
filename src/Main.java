794d637694cb0968b47808c2bbbaa67511568a36
ATU <thanhtudzx@gmail.com>
2023-09-05 08:40:34 +0700

    Frist Commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import common.Library;
+import controllers.SortingProgramming;
+import models.Element;
+/**
+ *
+ * @author Lenovo
+ */
+public class Main {
+    public static void main(String[] args) {
+        Library library = new Library();
+        Element element = new Element();
+        //1. Enter a possitive number
+        element.setSize_array(library.getInt("Enter number of array", 1, 100));
+        //2. Create array by generate random integer in range
+        element.setArray(library.createArray(element.size_array));
+        new SortingProgramming(element).run();
+    }
+}
